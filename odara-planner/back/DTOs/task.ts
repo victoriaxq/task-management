771994c118ccd7fc198b77a0e7cfc2fc28983991
Victória <victoriaxavierq@gmail.com>
2024-12-31 21:48:16 -0300

    estruturando pastas para manipulação de dados

--- odara-planner/back/DTOs/task.ts
+++ odara-planner/back/DTOs/task.ts
@@ -1,2 +1,22 @@
 import { z } from "zod";
 
+export enum Status {
+    TO_DO,
+    IN_PROGRESS,
+    DONE,
+}
+
+// model Task {
+//     id          Int     @id @default(autoincrement())
+//     title       String
+//     description String
+//     status      Status
+//   }
+
+// zod schema for Task model não deixa espaços em branco
+// o status vai ser uma string mas armazenado como um enum
+export const Task = z.object({
+    title: z.string().nonempty(),
+    description: z.string().nonempty(),
+    status: z.nativeEnum(Status),
+});
